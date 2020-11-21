23567f779a1e24689bcede8fca5a922a980a2cb4
Ubuntu <ubuntu@ip-172-31-95-146.ec2.internal>
2020-11-21 11:07:25 +0000

    add avatar onusers

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  has_one_attached :avatar   
+  
+  def avatar_thumbnail
+    if avatar.attached?
+      avatar.variant(resize: '150x150!').processed
+    end
+  end
+  
   validates :name, presence: true
   validates :age, presence: true
 end
